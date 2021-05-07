e63fccc4ccf1db237eb0aef5ee2b4b828c4db5cb
AshishBokil <ashishbkl11@gmail.com>
2021-05-07 23:43:15 +0530

    Added Wallet service

---
+++ src/main/java/pods/cabs/Wallet.java
@@ -0,0 +1,156 @@
+package pods.cabs;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.*;
+
+public class Wallet extends AbstractBehavior<Wallet.Command>
+{
+private int balance;
+private int initialBalance;
+private int id;
+
+
+public interface Command{}
+
+ 
+/*
+*  RESPONSE DEFINITION
+*/
+public static final class ResponseBalance
+{
+
+    int balance;
+    public  ResponseBalance(int balance)
+    {
+        this.balance=balance;
+    }
+}
+
+ /*
+     * INITIALIZATION
+     */
+    public static Behavior<Command> create(int id) {
+        return Behaviors.setup(context->{return new Wallet(context,id);});
+    }
+
+
+
+    private Wallet(ActorContext<Command> context , int id , int balance) { 
+        super(context);
+        this.id=id;
+        this.balance=0;
+        this.initialBalance=balance;
+    }
+
+    /*
+     * MESSAGE HANDLING
+     */
+
+     @Override
+     public Receive<Command> createReceive()
+     {
+         ReceiveBuilder<Command> builder = newReceiveBuilder();
+
+         builder.onMessage(GetBalance.class,    this::onGetBalance);
+         builder.onMessage(DeductBalance.class,    this::onDeductBalance);
+         builder.onMessage(AddBalance.class,    this::onAddBalance);
+         builder.onMessage(Reset.class,    this::onReset);
+
+         return builder.build();
+     }
+
+
+     private Behavior<Command> onGetBalance(GetBalance message)
+     {
+         message.replyTo.tell(new ResponseBalance(this.balance));
+         return this;
+     }
+
+     private Behavior<Command> onDeductBalance(DeductBalance message)
+     {
+         if(this.balance-message.deductvalue <0)
+         {
+             return -1;
+         }
+         this.balance-=message.deductvalue;
+         message.replyTo.tell(new ResponseBalance(this.balance));
+         return this;
+     }
+
+     private Behavior<Command> onAddBalance(AddBalance message)
+     {
+         this.balance+=message.balance;
+         return this;
+     }
+
+     private Behavior<Command> onReset(Reset message)
+     {
+
+        this.balance=initialBalance;
+        message.replyTo.tell(new ResponseBalance(this.balance));
+         return this;
+     }
+
+
+
+  /*
+     * COMMAND DEFINITIONS
+     */
+
+public static final class GetBalance implements Command{
+
+    final ActorRef<Wallet.ResponseBalance> replyTo;
+
+    public GetBalance(ActorRef<Wallet.ResponseBalance> replyTo)
+    {
+
+        this.replyTo=replyTo;
+    }
+
+}
+
+
+public static final class DeductBalance implements Command
+{
+   final ActorRef<Wallet.ResponseBalance> replyTo;
+    final int deductvalue;
+    public void DeductBalance(int toDeduct, ActorRef<Wallet.ResponseBalance> replyTo)
+    {
+
+        this.deductvalue=toDeduct;
+        this.replyTo=replyTo;
+    }
+}
+
+
+public static final class Reset implements Command
+{
+    final ActorRef<Wallet.ResponseBalance> replyTo;
+
+    public void Reset(ActorRef<Wallet.ResponseBalance> replyTo)
+    {
+       
+
+        this.replyTo=replyTo;
+    }
+
+
+}
+
+public static final class AddBalance implements Command
+{
+ 
+    final int balance;
+    public void AddBalance(int toAdd)
+    {
+        this.balance=toAdd;
+
+    }
+}
+
+
+
+
+}
+
